bb2d752b517509556c4447755d3b263db7573675
Xolas <nicolas.steck@hotmail.com>
2017-10-24 23:14:31 -0200

    Add files via upload

--- JS/navbar.js
+++ JS/navbar.js
@@ -5,50 +5,72 @@
 	// ONLOAD RESPONSIVE
 	window.onload = function() {
 
-			
-		if(window.innerWidth<=700)
-		{
-			if(document.getElementById("logo-li").style.display == "")
-				document.getElementById("logo-li").style.display = "none";
-		}
-		else
-		{
-			if(document.getElementById("logo-li").style.display == "none")
-				document.getElementById("logo-li").style.display = "";
-		}
-
 		if(window.innerWidth<=860)
 		{
 			var marg = window.innerWidth/50;
 
 			document.getElementById("logo-li").style.marginRight = marg+"%";
+		}
+
+		if(window.innerWidth>700) {
+
+			document.getElementById("navbar-icon").style.display = "none";
+		}
+		
+		if(window.innerWidth<700) {
+			var lista = document.getElementsByClassName("navbar-btn");
+			
+			for(j=0;j<4;j++) {
+
+				lista[j].style.display = "none";
+			}
 		}
 	};
 
+	
 	//RESIZE RESPONSIVE
 	window.onresize = function() {
 
+		var lista = document.getElementsByClassName("navbar-btn");
 			
-		if(window.innerWidth<=700)
-		{
-			if(document.getElementById("logo-li").style.display == "")
-				document.getElementById("logo-li").style.display = "none";
-		}
-		else
-		{
-			if(document.getElementById("logo-li").style.display == "none")
-				document.getElementById("logo-li").style.display = "";
+		if(window.innerWidth<=700) {
+
+			document.getElementById("navbar-icon").style.display = "inline-block";
+			
+			for(j=0;j<4;j++) {
+
+				lista[j].style.display = "none";
+			}
+
+		} else {
+
+			document.getElementById("navbar-icon").style.display = "none";
+
+			for(j=0;j<4;j++) {
+				lista[j].style.display = "inline-block"
+				lista[j].style.width = "122px"
+
+
+			}
 		}
 
-		if(window.innerWidth<=860)
-		{
+		if(window.innerWidth<=860) {
 			var marg = window.innerWidth/50;
 
 			document.getElementById("logo-li").style.marginRight = marg+"%";
-		}
-		else
-		{
+		} else {
+
 			document.getElementById("logo-li").style.marginRight = "30%";
+
+		}
+
+		if(window.innerWidth<700) {
+			var lista = document.getElementsByClassName("navbar-btn");
+			
+			for(j=0;j<4;j++) {
+
+				lista[j].style.display = "none";
+			}
 		}
 	};
 
@@ -58,8 +80,8 @@
 
 		var lista = document.getElementsByClassName("navbar-btn");
 		
-		for(j=0;j<4;j++)
-		{
+		for(j=0;j<4;j++) {
+
 			if(lista[j].classList.contains('active')) {
 			
 				lista[j].classList.remove('active');
@@ -70,4 +92,26 @@
 		esse.setAttribute("class","active navbar-btn");
 	}
 
+//]
+//SHOW NAVBAR IN LIST FORMAT
+//[
+	function showNavbar() {
+
+		var lista = document.getElementsByClassName("navbar-btn");
+		if(lista[1].style.display == "none") {
+			for(j=0;j<4;j++) {
+
+				lista[j].style.display = "";
+				lista[j].style.width = "100%";
+			}
+
+		} else {
+
+			for(j=0;j<4;j++) {
+
+				lista[j].style.display = "none";
+			}
+		}
+
+	}
 //]
